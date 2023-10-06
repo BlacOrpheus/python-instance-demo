7e798a6e65d4fcc9ce12d6226e9f3cc703cd6777
BlacOrpheus <103933543+BlacOrpheus@users.noreply.github.com>
2023-10-06 10:50:21 +0100

    Create my_script.py

---
+++ my_script.py
@@ -0,0 +1,13 @@
+import time
+import random
+from sympy import isprime
+
+def main():
+    while True:
+        num = random.randint(1,100)
+        prime_status = "prime" if isprime(num) else "not prime"
+        print(f"(num) is (prime_status).")
+        time.sleep(10)
+
+if _name_ == "_main_":
+    main()
